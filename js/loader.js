dcebe85cbaf6b137a3d7ff609eff3649589baac3
Fernando Buono <fbuono@fi.uba.ar>
2021-10-24 19:57:34 -0300

    [UPDATE] mas estilos a las cards

--- js/loader.js
+++ js/loader.js
@@ -6,7 +6,7 @@ $(window).on("load", () => {
     if(link !== 'index.html') {
         $('body').css('height', 'auto');
         $('body').css('overflow', 'auto');
-    }
+    };
     loader.css('opacity', '0');
     setTimeout(() => {
         loader.css('display', 'none');
