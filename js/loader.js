1ea013f40ec69d5076e8146a6782e34edcbb2b41
Fernando Buono <fbuono@fi.uba.ar>
2021-10-24 20:26:48 -0300

    [FIX] arreglado problema con scrollbars en el index

--- js/loader.js
+++ js/loader.js
@@ -3,7 +3,7 @@ const loader = $('#loader');
 const link = window.location.pathname.split('/').pop();
 
 $(window).on("load", () => {
-    if(link !== 'index.html' || link !== '') {
+    if(link !== 'index.html' && link !== '') {
         $('body').css('height', 'auto');
         $('body').css('overflow', 'auto');
     };
